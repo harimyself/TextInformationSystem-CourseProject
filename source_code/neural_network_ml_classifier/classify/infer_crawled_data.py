e5522128911b9376e68554e69d88cd9585899797
Hari <hbojja@equinix.com>
2020-12-11 19:53:22 -0800

    made path changes to infer scirpt

--- source_code/neural_network_ml_classifier/classify/infer_crawled_data.py
+++ source_code/neural_network_ml_classifier/classify/infer_crawled_data.py
@@ -15,14 +15,14 @@ from source_code.neural_network_ml_classifier.data_processor.PreProcess import P
         model_base_path: Path to tensorflow ML model fully trained and saved.
 """
 crawled_data_path = '../../Crawl-n-Extract/Merge/UIUC.txt'
-model_base_path = '../fully_trained_model/neural_network_model_v1'
+model_base_path = '../fully_trained_model/'
 
 
-print("Loading trained model from: ", model_base_path + '/model')
-model:Sequential = tf.keras.models.load_model(model_base_path + '/model')
+print("Loading trained model from: ", model_base_path + '/neural_network_model_v1/model')
+model:Sequential = tf.keras.models.load_model(model_base_path + '/neural_network_model_v1/model')
 
-print("Loading vectorized from: ", model_base_path + 'vectorizer/vectorizer_object')
-vectorizer:TfidfVectorizer = pickle.load(open(model_base_path + 'vectorizer/vectorizer_object', 'rb'))
+print("Loading vectorized from: ", model_base_path + '/vectorizer/vectorizer_object')
+vectorizer:TfidfVectorizer = pickle.load(open(model_base_path + '/vectorizer/vectorizer_object', 'rb'))
 
 print("Loading trained models completed..")
 
